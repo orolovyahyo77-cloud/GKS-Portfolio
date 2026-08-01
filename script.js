c85e97ba93c0d9df391eaf468df050652b804e9d
Yahyobek <orolovyahyo77@gmail.com>
2026-08-01 11:03:23 +0500

    Portfolio updated

--- script.js
+++ script.js
@@ -10,3 +10,22 @@ document.querySelectorAll(".card").forEach(card=>{
 window.addEventListener("load", () => {
     document.body.style.opacity = "1";
 });
+const btn = document.getElementById("theme-toggle");
+
+btn.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+
+    if(document.body.classList.contains("dark-mode")){
+        btn.innerHTML = "☀️ Light Mode";
+    }else{
+        btn.innerHTML = "🌙 Dark Mode";
+    }
+});
+document.querySelectorAll('a[href^="#"]').forEach(anchor=>{
+    anchor.addEventListener("click",function(e){
+        e.preventDefault();
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior:"smooth"
+        });
+    });
+});
